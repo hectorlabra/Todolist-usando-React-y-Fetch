a1a7d2ed44e201c8e72330d4110d8f01daaa3ad6
hectorlabra <121694325+hectorlabra@users.noreply.github.com>
2023-08-29 18:10:30 -0400

    integrating api succesfully

--- src/js/component/TaskList.jsx
+++ src/js/component/TaskList.jsx
@@ -2,11 +2,16 @@ import React from "react";
 import Task from "./Task";
 
 const TaskList = ({ tasks, onTaskToggle, onTaskDelete }) => {
+  // Verifica si tasks es un array antes de mapearlo
+  if (!Array.isArray(tasks)) {
+    return null; // O puedes manejar este caso de otra manera
+  }
+
   return (
     <ul className="list-group">
       {tasks.map((task) => (
         <Task
-          key={task.id}
+          key={task.id} // o key={task.otraPropiedadUnica} si tienes una propiedad única diferente
           task={task}
           onTaskToggle={onTaskToggle}
           onTaskDelete={onTaskDelete}
--- src/js/component/app.jsx
+++ src/js/component/app.jsx
@@ -7,8 +7,52 @@ class App extends Component {
     this.state = {
       tasks: [],
       newTaskText: "",
-      tasksLeft: 0, // Nuevo estado para el contador
+      tasksLeft: 0,
+      apiLoaded: false,
     };
+    this.apiUrl =
+      "https://playground.4geeks.com/apis/fake/todos/user/hectorlabra";
+  }
+
+  componentDidMount() {
+    this.fetchTasks();
+  }
+
+  fetchTasks() {
+    // Realizar una solicitud POST para crear el usuario solo si aún no se ha creado
+    if (!this.state.apiLoaded) {
+      fetch(this.apiUrl, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify([]),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log("Usuario creado:", data);
+          // Actualizar el estado para indicar que la API se ha cargado
+          this.setState({ apiLoaded: true });
+          // Luego, puedes continuar con las solicitudes GET y PUT
+          this.fetchTasks();
+        })
+        .catch((error) => {
+          console.error("Error al crear usuario:", error);
+        });
+    } else {
+      // Realizar la solicitud GET para obtener las tareas
+      fetch(this.apiUrl)
+        .then((response) => response.json())
+        .then((data) => {
+          this.setState({
+            tasks: data,
+            tasksLeft: data.filter((task) => !task.done).length,
+          });
+        })
+        .catch((error) => {
+          console.error("Error al obtener tareas:", error);
+        });
+    }
   }
 
   handleInputChange = (event) => {
@@ -17,51 +61,113 @@ class App extends Component {
 
   handleTaskAdd = (event) => {
     event.preventDefault();
-    const { tasks, newTaskText } = this.state;
+    const { newTaskText } = this.state;
     if (newTaskText.trim() !== "") {
       const newTask = {
-        id: Date.now(),
-        text: newTaskText,
-        active: true,
+        label: newTaskText,
+        done: false,
       };
+
       this.setState((prevState) => ({
         tasks: [...prevState.tasks, newTask],
         newTaskText: "",
-        tasksLeft: prevState.tasksLeft + 1, // Incrementar el contador
+        tasksLeft: prevState.tasksLeft + 1,
       }));
+
+      fetch(this.apiUrl, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify([...this.state.tasks, newTask]),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log("Tarea agregada:", data);
+        })
+        .catch((error) => {
+          console.error("Error al agregar tarea:", error);
+        });
     }
   };
 
   handleTaskToggle = (taskId) => {
     const updatedTasks = this.state.tasks.map((task) => {
-      if (task.id === taskId) {
-        task.active = !task.active;
+      if (task.label === taskId) {
+        task.done = !task.done;
       }
       return task;
     });
 
-    const activeTasks = updatedTasks.filter((task) => task.active);
-
     this.setState({
       tasks: updatedTasks,
-      tasksLeft: activeTasks.length, // Actualiza el contador
+      tasksLeft: updatedTasks.filter((task) => !task.done).length,
     });
+
+    fetch(this.apiUrl, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updatedTasks),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Tarea completada:", data);
+      })
+      .catch((error) => {
+        console.error("Error al completar tarea:", error);
+      });
   };
 
   handleTaskDelete = (taskId) => {
-    const { tasks } = this.state;
-    const updatedTasks = tasks.filter((task) => task.id !== taskId);
-
-    const activeTasks = updatedTasks.filter((task) => task.active);
+    const updatedTasks = this.state.tasks.filter(
+      (task) => task.label !== taskId
+    );
 
     this.setState({
       tasks: updatedTasks,
-      tasksLeft: activeTasks.length, // Actualiza el contador
+      tasksLeft: updatedTasks.filter((task) => !task.done).length,
     });
+
+    fetch(this.apiUrl, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updatedTasks),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Tarea eliminada:", data);
+      })
+      .catch((error) => {
+        console.error("Error al eliminar tarea:", error);
+      });
+  };
+
+  handleClearAllTasks = () => {
+    fetch(this.apiUrl, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then(() => {
+        this.setState({
+          tasks: [],
+          tasksLeft: 0,
+        });
+        console.log("Todas las tareas eliminadas");
+      })
+      .catch((error) => {
+        console.error("Error al eliminar todas las tareas:", error);
+      });
   };
 
   render() {
-    const { tasks, newTaskText, tasksLeft } = this.state;
+    const { tasks, newTaskText, tasksLeft, apiLoaded } = this.state;
     return (
       <div className="container mt-5">
         <h1>TodoList</h1>
@@ -81,11 +187,23 @@ class App extends Component {
             </div>
           </div>
         </form>
-        <TaskList
-          tasks={tasks}
-          onTaskToggle={this.handleTaskToggle}
-          onTaskDelete={this.handleTaskDelete}
-        />
+        {apiLoaded ? (
+          <div>
+            <TaskList
+              tasks={tasks}
+              onTaskToggle={this.handleTaskToggle}
+              onTaskDelete={this.handleTaskDelete}
+            />
+            <button
+              className="btn btn-danger"
+              onClick={this.handleClearAllTasks}
+            >
+              Limpiar tareas
+            </button>
+          </div>
+        ) : (
+          <p>Cargando tareas...</p>
+        )}
         {tasksLeft === 1 ? <p>1 Item left</p> : <p>{tasksLeft} Items left</p>}
       </div>
     );
